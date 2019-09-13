8f88d826d429f0a8567aff937421fae5dc21623e
nunzio giulio caggegi <nunziogiuliocaggegi@gmail.com>
2019-09-13 10:15:42 +0200

    Initial Commit

---
+++ The Beer Project - SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  The Beer Project - SwiftUI
+//
+//  Created by nunzio giulio caggegi on 13/09/2019.
+//  Copyright © 2019 nunzio giulio caggegi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
